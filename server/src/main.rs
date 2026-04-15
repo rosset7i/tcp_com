873c9fde7bcaf5e5cb30d32c72dad8bae561e40c
Matheus Rossetti <mh.rossetti2002@gmail.com>
2026-04-15 19:27:43 -0300

    feat: some improvements in mpsc error handling

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,27 +1,31 @@
 use std::{error::Error, net::SocketAddr, sync::Arc};
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
-    net::{TcpListener, tcp::OwnedReadHalf},
+    net::{
+        TcpListener,
+        tcp::{OwnedReadHalf, OwnedWriteHalf},
+    },
     sync::{
         Mutex,
-        mpsc::{self, UnboundedSender},
+        mpsc::{UnboundedReceiver, UnboundedSender, unbounded_channel},
     },
 };
+
+type ConnectedUser = (UnboundedSender<Vec<u8>>, SocketAddr);
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let listener = TcpListener::bind("0.0.0.0:8080").await?;
     println!("Server listening on: {}", listener.local_addr()?);
 
-    let clients: Arc<Mutex<Vec<(UnboundedSender<Vec<u8>>, SocketAddr)>>> =
-        Arc::new(Mutex::new(Vec::new()));
+    let clients: Arc<Mutex<Vec<ConnectedUser>>> = Arc::new(Mutex::new(Vec::new()));
 
     loop {
         let (stream, addr) = listener.accept().await?;
         println!("Accepted connection from: {}", addr);
 
-        let (sender, mut receiver) = mpsc::unbounded_channel::<Vec<u8>>();
-        let (reader, mut writer) = stream.into_split();
+        let (sender, receiver) = unbounded_channel::<Vec<u8>>();
+        let (reader, writer) = stream.into_split();
         {
             let mut lock = clients.lock().await;
             lock.push((sender, addr));
@@ -30,18 +34,13 @@ async fn main() -> Result<(), Box<dyn Error>> {
         let clients_for_thread = Arc::clone(&clients);
 
         tokio::spawn(async move { handle_connection(reader, clients_for_thread, addr).await });
-        tokio::spawn(async move {
-            loop {
-                let teste = receiver.recv().await.unwrap();
-                writer.write_all(&teste).await.unwrap();
-            }
-        });
+        tokio::spawn(async move { handle_broadcast(receiver, writer).await });
     }
 }
 
 async fn handle_connection(
     mut reader: OwnedReadHalf,
-    clients: Arc<Mutex<Vec<(UnboundedSender<Vec<u8>>, SocketAddr)>>>,
+    clients: Arc<Mutex<Vec<ConnectedUser>>>,
     current_client_addr: SocketAddr,
 ) {
     let mut buf = [0u8; 1024];
@@ -69,4 +68,13 @@ async fn handle_connection(
     }
 
     println!("{} was disconnected!", current_client_addr);
+}
+
+async fn handle_broadcast(mut receiver: UnboundedReceiver<Vec<u8>>, mut writer: OwnedWriteHalf) {
+    while let Some(bytes) = receiver.recv().await {
+        if writer.write_all(&bytes).await.is_err() {
+            let _ = writer.shutdown().await;
+            receiver.close();
+        }
+    }
 }
